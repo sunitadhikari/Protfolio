188144e046d6366dd09a672f5f45d1f621198c34
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-08 12:47:57 +0545

    gkj

--- prot.js
+++ prot.js
@@ -2,6 +2,7 @@
 const activePage = window.location.pathname;
 const navLinks= document.querySelectorAll('nav a').forEach(links =>{
    if(links.href.includes(`${activePage}`)){
-    console.log(`${activePage}`)
+        link.classList.add('active');
+    
 }  
 })
