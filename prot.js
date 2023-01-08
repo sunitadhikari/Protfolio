b0cdfc3c9f9975db1c19849045d0dceda7a491d3
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-08 13:07:52 +0545

    gkjkj

--- prot.js
+++ prot.js
@@ -1,8 +1,37 @@
 
-const activePage = window.location.pathname;
-const navLinks= document.querySelectorAll('nav a').forEach(links =>{
-   if(links.href.includes(`${activePage}`)){
-        link.classList.add('active');
+// const activePage = window.location.pathname;
+// const navLinks= document.querySelectorAll('nav a').forEach(links =>{
+//    if(links.href.includes(`${activePage}`)){
+//         link.classList.add('active');
     
-}  
-})
+// }  
+// })
+
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+     if (target.length) {
+         event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+         var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { 
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); 
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
