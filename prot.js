1ffc26cd1491f20d3ff14ca58b9755e928ab8d71
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-08 13:54:11 +0545

    gkj

--- prot.js
+++ prot.js
@@ -7,31 +7,44 @@
 // }  
 // })
 
-$('a[href*="#"]')
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-     if (target.length) {
-         event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-         var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { 
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); 
-            $target.focus();
-          };
-        });
-      }
-    }
+
+
+// $('a[href*="#"]')
+//   .not('[href="#"]')
+//   .not('[href="#0"]')
+//   .click(function(event) {
+//     if (
+//       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+//       && 
+//       location.hostname == this.hostname
+//     ) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//      if (target.length) {
+//          event.preventDefault();
+//         $('html, body').animate({
+//           scrollTop: target.offset().top
+//         }, 1000, function() {
+//          var $target = $(target);
+//           $target.focus();
+//           if ($target.is(":focus")) { 
+//             return false;
+//           } else {
+//             $target.attr('tabindex','-1'); 
+//             $target.focus();
+//           };
+//         });
+//       }
+//     }
+//   });
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const section = document.querySelector(event.target.getAttribute('href'));
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  const links = document.querySelectorAll('a');
+  links.forEach(link => {
+    link.addEventListener('click', scrollToSection);
   });
